aef37e25afa927ee438c80e66a41241070fb5a61
Roman Lukachyk <petrovroman26@icloud.com>
2023-03-02 15:27:52 +0200

    add animation for header bg

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 472) {
+    header.classList.add("header--scrolled");
+  } else {
+    header.classList.remove("header--scrolled");
+  }
+});
 customSelect("select");
